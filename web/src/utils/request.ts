3f06017d9cfced425395e64a57103b68cfb8b542
linjy <linxx5599@163.com>
2023-01-18 22:18:20 +0800

    web: change request

--- web/src/utils/request.ts
+++ web/src/utils/request.ts
@@ -1,8 +1,7 @@
 export default function (...arg: any) {
     return new Promise((resolve, reject) => {
-        fetch(...arg).then(res => res.json()).then(resilt => {
-            resolve(resilt)
-            resolve(resilt)
+        fetch(...arg).then(res => res.json()).then(result => {
+            resolve(result)
         }).catch(err => {
             reject(err)
         })
