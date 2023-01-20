2af67b9243a1df485c5d2bbc9dabc43945ed4f2d
linjy <linxx5599@163.com>
2023-01-20 16:53:32 +0800

    web: 添加css module

--- web/src/pages/Login/index.tsx
+++ web/src/pages/Login/index.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { Form, Button, Input, message } from "antd";
-import "./index.less";
+import style from "./index.module.less";
 import { loginApi } from "@/api";
 import { setToken } from "@/utils/auth";
 import { useNavigate } from "react-router-dom";
-const Login: React.FC  = () => {
+const Login: React.FC = () => {
   const navigate = useNavigate();
   const onFinish = (values: any) => {
     loginApi
@@ -20,9 +20,9 @@ const Login: React.FC  = () => {
   };
 
   return (
-    <div className="login-container">
-      <div className="login">
-        <div className="header">登录</div>
+    <div className={style["login-container"]}>
+      <div className={style.login}>
+        <div className={style.header}>登录</div>
         <Form
           name="basic"
           labelCol={{ span: 8 }}
@@ -35,27 +35,27 @@ const Login: React.FC  = () => {
             name="name"
             rules={[{ required: true, message: "请输入用户名" }]}
           >
-            <div className="container">
+            <div className={style.container}>
               <Input type="text" placeholder="请输入用户名" />
-              <span className="left"></span>
-              <span className="right"></span>
-              <span className="top"></span>
-              <span className="bottom"></span>
+              <span className={style.left}></span>
+              <span className={style.right}></span>
+              <span className={style.top}></span>
+              <span className={style.bottom}></span>
             </div>
           </Form.Item>
           <Form.Item
             name="password"
             rules={[{ required: true, message: "请输入密码" }]}
           >
-            <div className="container1">
+            <div className={style.container1}>
               <Input type="password" placeholder="请输入密码" />
-              <span className="left"></span>
-              <span className="right"></span>
-              <span className="top"></span>
-              <span className="bottom"></span>
+              <span className={style.left}></span>
+              <span className={style.right}></span>
+              <span className={style.top}></span>
+              <span className={style.bottom}></span>
             </div>
           </Form.Item>
-          <Button className="btn" type="primary" htmlType="submit">
+          <Button className={style.btn} type="primary" htmlType="submit">
             登录
           </Button>
         </Form>
