191731c1627f8502e73f473e0297635d28ed2697
linjy <linxx5599@163.com>
2023-01-26 23:04:03 +0800

    serve： 提取为变量config

--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ const cookieParser = require("cookie-parser");
 const morgan = require("morgan");
 const logger = require("./logger");
 
-const indexRouter = require("./routes/index");
+// const indexRouter = require("./routes/index");
 const getUserInfoRouter = require("./routes/getUserInfo");
 const getUserRouter = require("./routes/getUser");
 const insertUser = require("./routes/insertUser");
@@ -20,17 +20,49 @@ const dayjs = require("dayjs");
 
 const server = http.createServer(app);
 
+const socketConfig = {
+  types: {
+    //用户上线
+    NOTICE_USER_ONLINE: "notice-user-online",
+    //用户已登录
+    NOTICE_LOGGED_IN: "notice-logged-in",
+    //send-user 发送信息给服务端通知
+    SEND_USER: "send-user"
+  }
+};
+
 // 创建实时连接
 const io = new ioServer(server, {
   cors: {
     origin: "*"
   }
 });
+//在线人数的用户
+let userOnlineData = [];
 // 监听连接
-io.on("connection", (socket) => {
+io.on("connection", async (socket) => {
   console.log(`⚡: ${socket.id} 用户已连接!`);
-  socket.emit("message", "message");
+  socket.on(socketConfig.types.SEND_USER, async ({ uuid, token }) => {
+    // console.log(uuid, "uuid", await User.all());
+    const loggedInItem = userOnlineData.find(
+      (i) => i.uuid === uuid && i.token !== token
+    );
+    //其他用户登录已登录的用户
+    if (loggedInItem) {
+      socket.emit(socketConfig.types.NOTICE_LOGGED_IN, loggedInItem);
+      return;
+    }
+    if (!userOnlineData.some((i) => i.uuid === uuid)) {
+      socket.uuid = uuid;
+      userOnlineData.push({ socketId: socket.id, uuid, token });
+    }
+    io.emit(
+      socketConfig.types.NOTICE_USER_ONLINE,
+      userOnlineData.map(({ socketId, uuid }) => ({ socketId, uuid }))
+    );
+  });
   socket.on("disconnect", () => {
+    userOnlineData = userOnlineData.filter((i) => i.uuid !== socket.uuid);
     console.log("🔥: 一个用户已断开连接");
   });
 });
@@ -45,7 +77,7 @@ app.use(require("cors")());
 // unless指定哪些接口不需要访问权限，即白名单。
 app.use(
   expressjwt({ secret: JWT_SELECT, algorithms: ["HS256"] }).unless({
-    path: ["/login", /io/, "/"]
+    path: ["/login"]
   })
 );
 
